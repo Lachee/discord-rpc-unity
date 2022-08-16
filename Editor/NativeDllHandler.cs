974730b841026752f0bff2414dae39dac5a655f1
Lachee <lachlan.h@hotmail.com>
2022-08-16 12:24:26 +1000

    Fixed IL2CPP not linking correctly

--- Editor/NativeDllHandler.cs
+++ Editor/NativeDllHandler.cs
@@ -10,7 +10,6 @@ namespace Lachee.Discord.Editor
 	[InitializeOnLoad]
 	internal sealed class DiscordNativeInstall
 	{
-
 		const bool ENABLED = false;
 
 		const string PLUGIN_PATH_86_64 = "Discord RPC/Plugins/x86_64";
@@ -36,90 +35,33 @@ namespace Lachee.Discord.Editor
 				}
 			}
 
-#pragma warning disable 0162
-			if (!ENABLED) return;
-
-			//We are not windows, cannot do anything
-#if !UNITY_STANDALONE
-		Debug.LogError("Cannot use the discord library because the natives do not support non-windows platforms yet.");
-		CleanRoot();
-		return;
-#endif
-
-			//Copy the library over
-			CopyLibrary();
-
-#pragma warning restore 0162
-
-		}
-
-		private static void CopyLibrary()
-		{
-#if UNITY_EDITOR_64
-			CopyLibrary(PLUGIN_PATH_86_64, PLUGIN_NAME);
-#else
-		CopyLibrary(PLUGIN_PATH_86, PLUGIN_NAME);
+#if !UNITY_2019_OR_NEWER
+			FixLinkerSettings();
 #endif
 		}
 
-		private static void CleanRoot()
-		{
-			string lib = Path.Combine(Application.dataPath + "/../", PLUGIN_NAME);
-			if (File.Exists(lib))
-			{
-				try
-				{
-					Debug.Log("Attempting to delete the old native '" + lib + "'");
-					File.Delete(lib);
-				}
-				catch (Exception e)
-				{
-					Debug.LogError("Failed to delete the old native: " + e.Message);
-				}
-			}
-		}
 
-		private static void CopyLibrary(string path, string file)
-		{
-			//Prepare the paths
-			string sourcePath = Path.Combine(Application.dataPath, path);
-			string destPath = Application.dataPath + "/../";
+		static void FixLinkerSettings()
+        {
+			string linkPath = "Packages/com.lachee.discordrpc/Runtime/DiscordRPC.xml";
+			string cscPath = "Packages/com.lachee.discordrpc/Runtime/csc.rsp";
+			bool needsRefresh = false;
 
-			string sourceFile = Path.Combine(sourcePath, file);
-			string destFile = Path.Combine(destPath, file);
-
-			//Make sure the path exists
-			if (!File.Exists(sourceFile))
-			{
-				Debug.LogWarning("Since you are below Unity 2017, the DLL needs to be copied over. Could not find the native dll '" + sourceFile + "' to copy. Make sure you build the library before use!");
+			if (!File.Exists(linkPath)) {
+				string linkContent = "<linker><assembly fullname=\"DiscordRPC\" preserve=\"all\" /></linker>";
+				File.WriteAllText(linkPath, linkContent);
+				needsRefresh = true;
 			}
 
-			//Make sure the file doesn't already exist
-			if (File.Exists(destFile))
+			if (needsRefresh || !File.Exists(cscPath))
 			{
-				//They have both been modified at the last time, probably best to just leave if.
-				if (File.GetLastWriteTime(sourceFile) == File.GetLastWriteTime(destFile))
-					return;
-
-				//Give a warning about restarting
-				Debug.Log("Since you are below Unity 2017, the DLL needs to be copied over. The file '" + destFile + "' already exists. Will override but will require a unity restart to take effect");
-			}
-			else
-			{
-				Debug.Log("Since you are below Unity 2017, the DLL needs to be copied over. The file '" + destFile + "' is being copied to work with your current unity settings");
+				string cscContent = $"/res:{linkPath}";
+				File.WriteAllText(cscPath, cscContent);
+				needsRefresh = true;
 			}
 
-			//Copy the file
-			try
-			{
-				File.Copy(sourceFile, destFile, true);
-				File.SetAttributes(destFile, File.GetAttributes(sourceFile) & ~FileAttributes.ReadOnly);
-				Debug.Log("Copy succesful");
-			}
-			catch (Exception e)
-			{
-				Debug.LogError("A exception occured while trying to copy the native: " + e.Message);
-			}
-		}
+			if (needsRefresh)
+				AssetDatabase.Refresh();
+        }
 	}
 }
