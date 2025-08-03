2cffdba4ed3b3938aebf4445ec5b289081072507
Lachee <lachlan.h@hotmail.com>
2025-08-03 12:13:48 +1000

    initial connection

--- Runtime/DiscordManager.cs
+++ Runtime/DiscordManager.cs
@@ -2,17 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DiscordManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+using DiscordRPC;
+using DiscordRPC.Logging;
+using Lachee.DiscordRPC.Logging;
 
-    // Update is called once per frame
-    void Update()
+namespace Lachee.DiscordRPC
+{
+    public class DiscordManager : MonoBehaviour
     {
-        
+        public static DiscordManager current { get; private set; }
+        public DiscordRpcClient client { get; private set; }
+
+        public string applicationId = "424087019149328395";
+
+		void Awake() 
+        {
+            current = this;
+        }
+
+        void Start() 
+        {
+            Initialize();
+        }
+
+        void OnDestroy() 
+        {
+            Deinitialize();
+        }
+
+        void Update() 
+        {
+            if (client == null) 
+                return;
+
+            client.Invoke();
+        }
+
+        public void Initialize()  {
+            if (client != null)
+                Deinitialize();
+
+            var logger = new UnityLogger() { Level = LogLevel.Trace };
+            client = new DiscordRpcClient(applicationId, logger: logger, autoEvents: false);
+            client.Initialize();
+        }
+
+        public void Deinitialize() {
+            client.Deinitialize();
+            client.Dispose();
+            client = null;
+        }
     }
 }
